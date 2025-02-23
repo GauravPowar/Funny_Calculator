331d2b649a2a9d3608b45b1ba492d55850f51390
Gaurav Powar <163163378+GauravPowar@users.noreply.github.com>
2025-02-23 22:29:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-/* script.js */
 let display = document.getElementById("display");
 let message = document.getElementById("message");
 let funnyMessages = ["Please use your brain", "Go back to primary school", "Really?", "That's embarrassing!", "Try harder!"];
+
+document.addEventListener("keydown", function(event) {
+    if (event.key.match(/[0-9+\-*/.=]/)) {
+        appendValue(event.key);
+    } else if (event.key === "Enter") {
+        calculate();
+    } else if (event.key === "Backspace") {
+        deleteLast();
+    } else if (event.key === "Escape") {
+        clearDisplay();
+    }
+});
 
 function appendValue(value) {
     display.value += value;
